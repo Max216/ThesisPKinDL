1d623a83dc7e310eb3bd2b36d9923556bb29fd44
Max <MaxGlockner@gmx.de>
2018-01-08 13:17:07 +0200

    fix

--- find_data.py
+++ find_data.py
@@ -3,33 +3,45 @@ This script finds data and stored the found data
 '''
 import word_resource
 import mydataloader
-
 from docopt import docopt
 
-def in_resource(res, p, h, lbl):
-	return res.word_resource_overlap(sent1, sent2):
+def in_resource(res, p, h):
+    return res.word_resource_overlap(p, h)
 
 def main():
     args = docopt("""Find data based on some criterion.
 
     Usage:
-        find_data.py inres <data_path> <resource_path> <resource_label>
+        find_data.py inres <data_path> <resource_path> <resource_label> <name_out>
     """)
 
     data_path = args['<data_path>']
     resource_path = args['<resource_path>']
-    resource_label = args['resource_label>']
+    resource_label = args['<resource_label>']
+    name_out = args['<name_out>']
 
     filter_fn = None
     if args['inres']:
     	filter_fn = in_resource
 
-	w_res = word_resource.WordResource(resource_path, interested_relations=['contradiction'])
-	def filter(p, h, lbl):
-		return filter_fn(w_res, p, h, lbl)
+    print('Create resource for:', resource_label, '...')
+    w_res = word_resource.WordResource(resource_path, interested_relations=[resource_label])
+    print('Loaded', len(w_res), 'word information.')
 
-    loaded_data = mydataloader.load_snli(data_path, filter_fn=None):
+    print('Load relevant data for resource ...')
+    cnt_relevant = 0
+    cnt_irrelevant = 0
+    with open(data_path) as f_in:
+        with open(name_out) as f_out:
+            for line in f_in:
+                p, h, lbl = mydataloader.extract_snli(line)
+                if lbl in mydataloader.index_to_tag and filter_fn(w_res, p, h):
+                    cnt_relevant += 1
+                    f_out.write(line)
+                else:
+                    cnt_irrelevant += 1
 
+    print('Loaded', cnt_relevant, 'samples. Not used:', cnt_irrelevant, 'samples.' )
 
 
 if __name__ == '__main__':
