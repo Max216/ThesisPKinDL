5c2546b99125eb83a0743e575e0640fb66ae0153
Max <MaxGlockner@gmx.de>
2018-03-07 16:05:21 +0200

    .

--- clean/scripts/wn_representation_trainer.py
+++ clean/scripts/wn_representation_trainer.py
@@ -245,7 +245,7 @@ def train(data_path, encoder_hidden_dim, encoder_out_dim, matcher_hidden_dim, ou
         for w in vocab:
             w_index = autograd.Variable(cuda_wrap(torch.LongTensor([embedding_holder.word_index(w)]).view(1,-1)))
             print(w_index.size())
-            embedding = matcher.embedding_encoder(w_index).cpu().numpy().list()
+            embedding = matcher.embedding_encoder(w_index).data.cpu().numpy().list()
             print(w + ' ' + ' '.join([str(v) for v in embedding]))
     
 
