0b77c9b150cd63bc89d20d8ccc7af1f787f785bf
Max <MaxGlockner@gmx.de>
2018-03-08 14:17:58 +0200

    .

--- clean/scripts/wn_representation_trainer.py
+++ clean/scripts/wn_representation_trainer.py
@@ -251,7 +251,7 @@ def train_cos(data_path, encoder_hidden_dim, encoder_out_dim, out_path, embeddin
 
 
 
-        return (-2 * only_contradiction.sum()) + only_entailment.sum()
+        return only_contradiction.sum() + (-1 * only_entailment.sum())
 
     for i in range(iterations):
         print('Train iteration:', i+1)
