76184da9a92a09d07f2db43b551fe04f2e610f45
Max <MaxGlockner@gmx.de>
2018-03-08 12:40:27 +0200

    .

--- clean/scripts/wn_representation_trainer.py
+++ clean/scripts/wn_representation_trainer.py
@@ -223,6 +223,8 @@ def train_cos(data_path, encoder_hidden_dim, encoder_out_dim, out_path, embeddin
         multiplicator_entailment = var_lbl.data.clone().fill_(-1) * var_lbl.data 
         multiplicator_contradiction = var_lbl.data.clone().fill_(1) - var_lbl.data
         multiplicator = autograd.Variable(multiplicator_entailment + multiplicator_contradiction, requires_grad=False)
+        print('pred',prediction.size())
+        print('mult', multiplicator.size())
         loss = prediction * multiplicator.float()
         return loss.sum()
 
