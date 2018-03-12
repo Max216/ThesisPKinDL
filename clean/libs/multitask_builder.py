164c7b7517de884ea0d32faf8e39cd67d2f56bb6
Max <MaxGlockner@gmx.de>
2018-03-12 18:40:56 +0200

    .

--- clean/libs/multitask_builder.py
+++ clean/libs/multitask_builder.py
@@ -164,7 +164,9 @@ class MultitaskBuilder:
 
     def loss(self, snli_loss, premise_info, hypothesis_info):
         """ Calculate the loss for thee gradient """
+        print('now create loss')
         multitask_loss = self._loss_fn_multitask(premise_info, hypothesis_info, self)
+        print('multitask loss', multitask_loss)
         return self._loss_fn(snli_loss, multitask_loss)
 
     def adjust_lr(self, new_lr):
