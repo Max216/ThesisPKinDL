fe548e07c468d5108580c1c7c680e5c631ff4f44
Max <MaxGlockner@gmx.de>
2018-03-14 10:59:24 +0200

    .

--- clean/libs/multitask.py
+++ clean/libs/multitask.py
@@ -216,6 +216,8 @@ class MultiTaskTarget:
                 target_labels[i] = labels.view(-1)
                 #print('pos adding')
 
+            print('added', target_words[i].size())
+
         self._target_words = target_words
         self._target_labels = target_labels
 
