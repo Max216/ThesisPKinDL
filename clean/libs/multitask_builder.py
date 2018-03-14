53303d5735362fb9ff746d646584fdd13d1396d9
Max <MaxGlockner@gmx.de>
2018-03-14 14:56:04 +0200

    .

--- clean/libs/multitask_builder.py
+++ clean/libs/multitask_builder.py
@@ -306,7 +306,7 @@ def loss_multitask_reweighted(premise_info, hypothesis_info, premise_ids, hyp_id
         print('predictions', predictions)
         #print('predicted', predictions.size())
         batch_loss = F.cross_entropy(predictions, lbl_var)
-        print(batch_loss)
+        loss.append(batch_loss)
         #return batch_loss
         #multiplicator_batch_factor = autograd.Variable(batch_loss.data.clone().fill_(batch_factor))
         #loss.append(batch_loss) #* multiplicator_batch_factor
