6c827c780235ccdf59f82030701e458eee3226c4
Max <MaxGlockner@gmx.de>
2018-03-15 13:42:05 +0200

    .

--- clean/scripts/extract_wordnet.py
+++ clean/scripts/extract_wordnet.py
@@ -206,8 +206,9 @@ def create_data_using_first_synset(vocab_path, out_path):
 
     with open(vocab_path) as f_in:
         vocab = [line.strip() for line in f_in.readlines()]
+        print('used vocab before filtering', vocab)
         vocab = [w for w in vocab if w not in spacy.en.language_data.STOP_WORDS and w.lower() not in spacy.en.language_data.STOP_WORDS]
-
+        print('used vocab after filtering:', vocab)
     vocab_set = set(vocab)
     result = []
     total_len = len(vocab)
