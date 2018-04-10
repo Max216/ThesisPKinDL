f87d915f409b2036a3a1bd2e665217772adc3f11
Max <MaxGlockner@gmx.de>
2018-04-10 13:20:35 +0200

    .

--- clean/libs/multitask_builder.py
+++ clean/libs/multitask_builder.py
@@ -901,6 +901,22 @@ def get_builder(classifier, mt_type, mt_target, lr, embedding_holder):
         params['regularization_update'] = dummy_regularization
 
         return MultitaskBuilder(params, lr, mt_target.get_targets(make_even_dist=False), classifier, embedding_holder)
+        
+    elif mt_type == 'mt_25_snli_ddropout_300_masking':
+        print('mt_25_snli_ddropout_300_masking')
+        # weight both results the same, all the time
+        params['multitask_network'] = get_multitask_nw_dropout(classifier, mlp=300)
+        params['optimizer'] = get_optimizer_multitask_only
+        params['loss_fn_multitask'] = loss_multitask_reweighted
+        params['loss_fn'] = loss_on_regularization
+        params['regularization_update'] = constant_25_percent
+        params['mask_sent'] = True
+
+        return MultitaskBuilder(params, lr, mt_target.get_targets(), classifier, embedding_holder)
+
+
+
+
         #return DataLoader(SentMTDataset(samples), drop_last=False, batch_size=512, shuffle=False, collate_fn=CollateBatchMultiTask()), len(samples)
         # samples = []
 
