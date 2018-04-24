0603ef23fa2251a7e10bf8ab7c587c071e8495bd
Max <MaxGlockner@gmx.de>
2018-04-24 12:08:05 +0200

    .

---
+++ clean/scripts/subm_analyse.py
@@ -0,0 +1,68 @@
+import sys, json, re
+sys.path.append('./../')
+
+from docopt import docopt
+
+import collections
+import numpy as np
+
+def main():
+    args = docopt("""Analyse creation and baselines for the submission. 
+
+    Usage:
+        subm_analyse.py word_dist <train_data> <newtest> <out>
+
+    """)
+
+    if args['word_dist']:
+        analyse_word_distribution(args['<train_data>'], args['<newtest>'], args['<out>'])
+
+
+def analyse_word_distribution(train_data, newtest, out_file):
+    print('Read train data ...')
+    with open(train_data) as f_in:
+        train = [json.loads(line.strip()) for line in f_in.readlines()]
+    print('Done.')
+
+    print('Read new test-set ...')
+    with open(newtest) as f_in:
+        test = [json.loads(line.strip()) for line in f_in.readlines()]
+    print('Done.')
+
+    print('Find replacements ...')
+    replacement_counter = collections.Counter([data['replaced2'] for data in test])
+    print('Most Common:')
+    print(replacement_counter.most_common()[:5])
+    print('Least common:', replacement_counter.most_common()[-3:])
+
+    print('Count replaced words in train data ...')
+    regexps = [((w, re.compile('\\b' + w + '\\b'))) for w in replacement_counter]
+    print('Created', len(regexps), 'regexp')
+    orig_counts = collections.defaultdict(int)
+    for data in train:
+        for w, regex in regexps:
+            if regex.search(data['sentence1']):
+                orig_counts[w] += 1
+            if regex.search(data['sentence2']):
+                orig_counts[w] += 1
+
+    print('Done')
+    only_orig_counts = sorted([orig_counts[w] for w in orig_counts])
+    print('maximum counts', only_orig_counts[-3:])
+    print('minimum counts:', only_orig_counts[:3])
+    print('mean:', np.mean(np.asarray(only_orig_counts)),'validate::' sum(only_orig_counts) / len(only_orig_counts))
+    print('median:', np.median(np.asarray(only_orig_counts)))
+
+    print('Write details out ...')
+    with open(out_file, 'w') as f_out:
+        counts = sorted([(w, orig_counts[w]) for w in orig_counts], key=lambda x: x[1])
+        print('#word\t#orig_count\t#replacements_count')
+        for w, c in counts:
+            f_out.write(w + '\t' + str(c) + '\t' + str(replacement_counter[w]) + '\n')
+
+    print('Done.')
+
+
+
+if __name__ == '__main__':
+    main()
