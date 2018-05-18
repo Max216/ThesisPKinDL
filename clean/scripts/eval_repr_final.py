ada30af122535296a74d07a7af9ebccd3e0a8e49
Max <MaxGlockner@gmx.de>
2018-05-18 17:17:17 +0200

    .

--- clean/scripts/eval_repr_final.py
+++ clean/scripts/eval_repr_final.py
@@ -17,36 +17,36 @@ def main():
         store_repr(args['<model_path>'], args['<dataset_path>'], args['<out_path>'])
 
 
-    def store_repr(model_path, data_path, out_path):
-        print('Load embeddings')
-        embedding_holder = embeddingholder.create_embeddingholder()
-        print('Load data')
-        dataholder = data_handler.Datahandler(data_path, data_format='snli', include_start_end_token=True)
-        print('Load model')
-        classifier_name, classifier, embedding_holder = model_tools.load(model_path, embedding_holder)
-        print('Predict')
+def store_repr(model_path, data_path, out_path):
+    print('Load embeddings')
+    embedding_holder = embeddingholder.create_embeddingholder()
+    print('Load data')
+    dataholder = data_handler.Datahandler(data_path, data_format='snli', include_start_end_token=True)
+    print('Load model')
+    classifier_name, classifier, embedding_holder = model_tools.load(model_path, embedding_holder)
+    print('Predict')
 
-        index_to_tag = data_tools.DEFAULT_VALID_LABELS
-        with open(out_path, 'w') as f_out:
-            data_loader = DataLoader(dataholder, drop_last=False, batch_size=1, shuffle=False, collate_fn=collatebatch.CollateBatch(padding_token))
-            for premise_batch, hyp_batch, lbl_batch in data_loader:
-                prediction, _, representations = classifier(
-                    autograd.Variable(premise_batch),
-                    autograd.Variable(hyp_batch),
-                    output_sent_info = True
-                )
+    index_to_tag = data_tools.DEFAULT_VALID_LABELS
+    with open(out_path, 'w') as f_out:
+        data_loader = DataLoader(dataholder, drop_last=False, batch_size=1, shuffle=False, collate_fn=collatebatch.CollateBatch(padding_token))
+        for premise_batch, hyp_batch, lbl_batch in data_loader:
+            prediction, _, representations = classifier(
+                autograd.Variable(premise_batch),
+                autograd.Variable(hyp_batch),
+                output_sent_info = True
+            )
 
-                _, predicted_idx = torch.max(scores.data, dim=1)
-                pred_label = index_to_tag[predicted_idx.data[0]]
-                gold_label = index_to_tag[lbl_batch.data[0]]
+            _, predicted_idx = torch.max(scores.data, dim=1)
+            pred_label = index_to_tag[predicted_idx.data[0]]
+            gold_label = index_to_tag[lbl_batch.data[0]]
 
-                premise_repr = representations[0][0].data
-                hyp_repr = representations[1][0].data
+            premise_repr = representations[0][0].data
+            hyp_repr = representations[1][0].data
 
-                print('pred:', pred_label)
-                print('gold:', gold_label)
-                print('premise_repr', premise_repr)
-                print('hyp_repr', hyp_repr)
+            print('pred:', pred_label)
+            print('gold:', gold_label)
+            print('premise_repr', premise_repr)
+            print('hyp_repr', hyp_repr)
 
 
 
